c91846a0b79d2bd17c58b83f66febf5c1b57320f
Angela Branaes <angela.branaes@gmail.com>
2020-04-23 21:17:34 +0100

    remove im:history permission

--- src/Main.js
+++ src/Main.js
@@ -17,7 +17,7 @@ export default class Main extends Component {
             and who it's time to reach out to again.
           </h4>
           <br></br>
-          <a href="https://slack.com/oauth/v2/authorize?client_id=1052632707317.1057131634261&scope=chat:write,files:write,users:read&user_scope=im:history,im:read"><img alt="Add to Slack" height="45" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/></a>
+          <a href="https://slack.com/oauth/v2/authorize?client_id=1052632707317.1057131634261&scope=chat:write,files:write,users:read&user_scope=im:read"><img alt="Add to Slack" height="45" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/></a>
           </div>
           <div>
             <img className="screenshot" src={ Screenshot } alt="screenshot of Chatty app" />
