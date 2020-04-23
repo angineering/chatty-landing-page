8191a844e8b4851516b4de161e6fce3fb3bb9ac2
Angela Branaes <angela.branaes@gmail.com>
2020-04-23 12:29:12 +0100

    Some improvement to pages

--- src/Contact.js
+++ src/Contact.js
@@ -6,21 +6,17 @@ export default class Contact extends Component {
       <div className="page">
         <h1>Contact</h1>
 
-        <section>
-          <h2>Support</h2>
-          <p>
-            If you have feedback, suggestions or issues with the Chatty Slack App,
-            please email the Chatty team on <a href='mailto:help@bechatty.app'>help@bechatty.app</a>
-          </p>
-        </section>
+        <h2>Support</h2>
+        <p>
+          If you have feedback, suggestions or issues with the Chatty Slack App,
+          please email the Chatty team on <a href='mailto:help@bechatty.app'>help@bechatty.app</a>
+        </p>
 
-        <section>
-          <h2>Feedback</h2>
-          <p>
-            If you have feedback or suggestions for the Chatty Slack App,
-            please email the Chatty team on <a href='mailto:hello@bechatty.app'>hello@bechatty.app</a>
-          </p>
-        </section>
+        <h2>Feedback</h2>
+        <p>
+          If you have feedback or suggestions for the Chatty Slack App,
+          please email the Chatty team on <a href='mailto:hello@bechatty.app'>hello@bechatty.app</a>
+        </p>
 
       </div>
     )
